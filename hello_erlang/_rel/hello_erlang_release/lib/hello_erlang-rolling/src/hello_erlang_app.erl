13c1610deb85eec7a0bad24b6cbba210fa336210
RandomSeeded <nate@blendlabs.com>
2017-02-28 09:01:06 -0800

    eh screw it everything

---
+++ hello_erlang/_rel/hello_erlang_release/lib/hello_erlang-rolling/src/hello_erlang_app.erl
@@ -0,0 +1,22 @@
+-module(hello_erlang_app).
+-behaviour(application).
+
+-export([start/2]).
+-export([stop/1]).
+
+start(_Type, _Args) ->
+    Dispatch = cowboy_router:compile([
+        {'_', [
+          {"/", cowboy_static, {priv_file, hello_erlang, "index.html"}},
+          {"/test", hello_handler, []},
+          {"/email-submit.html", cowboy_static, {priv_file, hello_erlang, "email-submit.html"}}
+        ]}
+    ]),
+    {ok, _} = cowboy:start_clear(my_http_listener, 100,
+        [{port, 8080}],
+        #{env => #{dispatch => Dispatch}}
+    ),
+    hello_erlang_sup:start_link().
+
+stop(_State) ->
+	ok.
