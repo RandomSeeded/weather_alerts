f47a746b737aaca7bd30fb007a240ec49d881862
RandomSeeded <nate@blendlabs.com>
2017-03-02 10:21:46 -0800

    basic 200

--- src/surf_alert_app.erl
+++ src/surf_alert_app.erl
@@ -5,7 +5,14 @@
 -export([stop/1]).
 
 start(_Type, _Args) ->
-	surf_alert_sup:start_link().
+    Dispatch = cowboy_router:compile([
+        {'_', [{"/", index_handler, []}]}
+    ]),
+    {ok, _} = cowboy:start_clear(my_http_listener, 100,
+        [{port, 8080}],
+        #{env => #{dispatch => Dispatch}}
+    ),
+    surf_alert_sup:start_link().
 
 stop(_State) ->
 	ok.
