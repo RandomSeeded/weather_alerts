7996759541e2c6ef171825cbff03d9e97705871b
nate willard <nate.a.willard@gmail.com>
2017-03-14 07:50:29 -0700

    mh cleanup

--- scripts/src/mongo_handler.erl
+++ scripts/src/mongo_handler.erl
@@ -4,10 +4,6 @@
 -record(db_info, {collection="emails",
     connection}).
 
-
-% This function is called by the supervisor
-% It is responsible for creating a mongo process via init
-% And returning the processId for that process to the supervisor
 start_link() ->
   gen_server:start_link(?MODULE, init, []).
 
@@ -28,29 +24,4 @@ handle_call(get_emails, _From, MyDB) ->
 
 get_emails(Pid) ->
   gen_server:call(Pid, get_emails).
-
-% This loop function listens from messages from our externally facing functions such as add_email
-% loop(MyDB) ->
-%   receive
-%     {shutdown} ->
-%       io:format("Shutdown~n"),
-%       ok;
-%     {Pid, MsgRef, {add_email, Email}} ->
-%       io:format("adding email~n"),
-%       Connection = MyDB#db_info.connection,
-%       Collection = <<"emails">>,
-%       mc_worker_api:insert(Connection, Collection, [#{<<"email">> => Email}]),
-%       Pid ! {MsgRef, ok},
-%       loop(MyDB);
-%     {Pid, MsgRef, get_emails} ->
-%       io:format("Retrieving emails...~n"),
-%       Connection = MyDB#db_info.connection,
-%       Collection = <<"emails">>,
-%       mc_worker_api:find(Connection, Collection, [#{}]),
-%       Pid ! {MsgRef, ok},
-%       loop(MyDB);
-%     Unknown ->
-%       io:format("Unknown message ~p~n", [Unknown]),
-%       loop(MyDB)
-%   end.
 
