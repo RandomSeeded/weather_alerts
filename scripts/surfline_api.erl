c958dc7731ec3d41caf1cdf1d0d6b9d276c6b7a5
nate willard <nate.a.willard@gmail.com>
2017-03-13 21:15:44 -0700

    surfline api now running in separate process

--- scripts/surfline_api.erl
+++ scripts/surfline_api.erl
@@ -16,8 +16,18 @@ start_link() ->
 % handle-cast: for ASYNC
 % Fundamentally get_forecast is for now a synchronous operation
 
-handle_call(get_forecast, _From, []) ->
-  {reply, "Test", []};
+handle_call(check_forecast_good, _From, []) ->
+  Body = get_forecast(),
+  Tomorrows_Forecast = decode_JSON(Body),
+  Response = case Tomorrows_Forecast of
+    "fair" ->
+      true;
+    "good" ->
+      true;
+    _ ->
+      false
+  end,
+  {reply, Response, []};
 handle_call(test, _From, []) ->
   {reply, "Test", []}.
 
@@ -26,6 +36,9 @@ handle_call(test, _From, []) ->
 test(Pid) ->
   gen_server:call(Pid, test).
 
+% OK THIS IS COOL AND ALL BUT IT'S NOT AN EXPOSED FUNCTION. (nuke it)
+% get_forecast(Pid) ->
+%   gen_server:call(Pid, get_forecast).
 get_forecast() ->
   inets:start(),
   Url = "http://api.surfline.com/v1/forecasts/2957?resources=analysis&days=2",
@@ -45,17 +58,7 @@ decode_JSON(Body) ->
 % Additionally, this should be made somehow fault tolerant.
 % Right now if the api call fails this thing just keels over and dies
 
-
-% Client methods
-check_forecast_good() ->
-  Body = get_forecast(),
-  Tomorrows_Forecast = decode_JSON(Body),
-  case Tomorrows_Forecast of
-    "fair" ->
-      true;
-    "good" ->
-      true;
-    _ ->
-      false
-  end.
+% Client methods (currently broken pending gen_server revamp)
+check_forecast_good(Pid) ->
+  gen_server:call(Pid, check_forecast_good).
 
