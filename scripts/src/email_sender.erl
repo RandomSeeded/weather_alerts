2b294cc8d5f8479b12f4eb2f0aad02eb3d0292a4
RandomSeeded <nate@blendlabs.com>
2017-03-15 18:36:18 -0700

    email sender now using gen_server

--- scripts/src/email_sender.erl
+++ scripts/src/email_sender.erl
@@ -1,21 +1,29 @@
 -module(email_sender).
--export([start/0, test/0]). 
+-compile(export_all).
 
-test() ->
-  ok.
+-behavior(gen_server).
 
-start() -> 
+start_link() ->
+  gen_server:start_link(?MODULE, [], []).
+
+% This function: initial setup and then loops
+init([]) ->
   application:start(crypto),
   application:start(asn1),
   application:start(public_key),
   application:start(ssl),
   {ok, Password} = file:read_file("../.passwords"),
-  Recipient = "nawnate@gmail.com",
-  gen_smtp_client:send_blocking({"SurfAlertMailer@gmail.com",
+  {ok, Password}.
+
+handle_cast({send_email, Recipient}, Password) ->
+  gen_smtp_client:send({"SurfAlertMailer@gmail.com",
       [Recipient], "Subject: New Test123456"},
     [{relay, "smtp.gmail.com"},
       {ssl, true},
       {username, "SurfAlertMailer@gmail.com"},
       {password, erlang:binary_to_list(Password)}]),
-  erlang:halt(0).
+  {noreply, Password}.
+
+send_email(Pid, Recipient) ->
+  gen_server:cast(Pid, {send_email, Recipient}).
 
