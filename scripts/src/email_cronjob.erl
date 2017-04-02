adb048a985b0f926e3a2fec7ecf087954167aea2
nate willard <nate.a.willard@gmail.com>
2017-04-02 15:13:51 -0700

    progress

--- scripts/src/email_cronjob.erl
+++ scripts/src/email_cronjob.erl
@@ -1,6 +1,7 @@
 -module(email_cronjob).
 -behavior(gen_server).
 -compile(export_all).
+-include("surfline_definitions.hrl").
 
 % What should this look like?
 % It will call the surfline API (this should be a process)
@@ -21,28 +22,47 @@ send_emails(_) ->
   ok.
 
 init([]) ->
-  {ok, SurflinePid} = surfline_api:start_link(),
-  {ok, SurflinePid}.
+  % {ok, SurflinePid} = surfline_api:start_link(),
+  {ok, []}.
 
 start_link() ->
   gen_server:start_link(?MODULE, [], []).
 
-internal_run(SurflinePid) ->
+% OK so this needs to be modified to have multi-region support.
+% What do we want architecture to look like?
+% We invoke API function single time.
+% It loops through all regions
+% For each region it spawns a separate surfline-checking process, and waits for a repsonse
+% It then invokes send_emails based on that response
+internal_run() ->
   io:format("Checking Forecast~n"),
+  % Now we need to make this lists:each style.
+  % Sync? Async?
+  % It's easiest to leave this sync, but then if one fails they all fail, which is shit city.
+  % Additionally, the map is synchronous, which is also shit city.
+  SurflineDefinitions = ?Surfline_definitions,
+  io:format("SurflineDefinitions ~p~n", [SurflineDefinitions]),
+  lists:foreach(fun(N) ->
+                    io:format("N ~p~n", [N])
+                end, ?Surfline_definitions),
+  {ok, SurflinePid} = surfline_api:start_link(),
   Forecast = surfline_api:check_forecast_good(SurflinePid),
+  % So fundamentally we need to start a new process for each caller, and tell that process to do shit.
+  % This is not a good clean model because these processes won't necessarily exit, this is shit city.
+  % Instead what we would probably want would be a pool of surfline API check-ers, and a pool of email senders. We would just send messages to each.
   send_emails(Forecast).
 
-internal_run_repeat(SurflinePid, Delay) ->
-  internal_run(SurflinePid),
+internal_run_repeat(Delay) ->
+  internal_run(),
   timer:sleep(Delay * 3600000), % convert to hours
-  internal_run_repeat(SurflinePid, Delay).
+  internal_run_repeat(Delay).
 
-handle_cast(run, SurflinePid) ->
-  internal_run(SurflinePid),
-  {noreply, SurflinePid};
-handle_cast({run_repeat, Delay}, SurflinePid) ->
-  internal_run_repeat(SurflinePid, Delay),
-  {noreply, SurflinePid}.
+handle_cast(run, State) ->
+  internal_run(),
+  {noreply, State};
+handle_cast({run_repeat, Delay}, State) ->
+  internal_run_repeat(Delay),
+  {noreply, State}.
   
 run(Pid) ->
   gen_server:cast(Pid, run).
