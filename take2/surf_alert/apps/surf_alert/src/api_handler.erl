3cbe970359b31cf0f9f78d35d5bb4e4667551627
nate willard <nate.a.willard@gmail.com>
2017-04-06 20:17:59 -0700

    cowboy 2.0

---
+++ take2/surf_alert/apps/surf_alert/src/api_handler.erl
@@ -0,0 +1,29 @@
+-module(api_handler).
+-behavior(cowboy_handler).
+
+-export([init/2]).
+
+init(Req0, State) ->
+    Method = cowboy_req:method(Req0),
+      Path = cowboy_req:path(Req0),
+        Req = handle(Method, Path, Req0),
+          {ok, Req, State}.
+
+handle(<<"POST">>, <<"/api/email-submit">>, Req0) ->
+    {ok, KeyValues, Req} = cowboy_req:read_urlencoded_body(Req0),
+      Email = proplists:get_value(<<"email">>, KeyValues),
+        Region = proplists:get_value(<<"region">>, KeyValues),
+          mongo_handler:add_email(Email, Region),
+            cowboy_req:reply(200, #{
+                  <<"content-type">> => <<"text/plain">>
+                    }, "OK", Req);
+handle(<<"POST">>, <<"/api/email-unsubscribe">>, Req0) ->
+    {ok, KeyValues, Req} = cowboy_req:read_urlencoded_body(Req0),
+      Email = proplists:get_value(<<"email">>, KeyValues),
+        mongo_handler:remove_email(Email),
+          cowboy_req:reply(200, #{
+                <<"content-type">> => <<"text/plain">>
+                  }, "OK", Req);
+handle(_Method, _Path, Req) ->
+    cowboy_req:reply(405, Req).
+
--- take2/surf_alert/apps/surf_alert/src/surf_alert_app.erl
+++ take2/surf_alert/apps/surf_alert/src/surf_alert_app.erl
@@ -36,15 +36,14 @@
 start(_StartType, _StartArgs) ->
     Dispatch = cowboy_router:compile([
         {'_', [
-          % {"/", cowboy_static, {priv_file, surf_alert, "index.html"}},
-          % {"/api/email-submit", api_handler, []}
-          {"/api/email-submit", toppage_handler, []}
+          {"/", cowboy_static, {priv_file, surf_alert, "index.html"}},
+          {"/api/email-submit", api_handler, []}
           % {"/api/email-unsubscribe", api_handler, []},
           % {"/[...]", cowboy_static, {priv_dir, surf_alert, []}}
         ]}
     ]),
     {ok, _} = cowboy:start_clear(my_http_listener, 100,
-        [{port, 8081}],
+        [{port, 8080}],
         #{env => #{dispatch => Dispatch}}
     ),
     surf_alert_sup:start_link().
