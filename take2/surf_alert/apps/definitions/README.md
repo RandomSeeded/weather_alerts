4e8f31f7e7d3a16b460e9477280addd4fcf4dd39
RandomSeeded <nate@blendlabs.com>
2017-04-06 18:41:15 -0700

    rm unnecessary app for includes

--- take2/surf_alert/apps/definitions/README.md
+++
@@ -1,9 +0,0 @@
-definitions
-=====
-
-An OTP library
-
-Build
------
-
-    $ rebar3 compile
--- take2/surf_alert/apps/mongo_handler/src/mongo_handler.erl
+++ take2/surf_alert/apps/mongo_handler/src/mongo_handler.erl
@@ -4,6 +4,8 @@
 -record(db_info, {collection="emails",
   connection}).
 
+-include_lib("include/surfline_definitions.hrl").
+
 % By our supervisor
 start_link() ->
   gen_server:start_link(?MODULE, [], []).
