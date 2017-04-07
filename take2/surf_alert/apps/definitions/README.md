108428dc59436edf45f93b430857080cb14d8702
RandomSeeded <nate@blendlabs.com>
2017-04-06 18:23:36 -0700

    stuff

---
+++ take2/surf_alert/apps/definitions/README.md
@@ -0,0 +1,9 @@
+definitions
+=====
+
+An OTP library
+
+Build
+-----
+
+    $ rebar3 compile
--- take2/surf_alert/apps/mongo_handler/src/mongo_handler.erl
+++ take2/surf_alert/apps/mongo_handler/src/mongo_handler.erl
@@ -4,9 +4,11 @@
 -record(db_info, {collection="emails",
   connection}).
 
+% By our supervisor
 start_link() ->
   gen_server:start_link(?MODULE, [], []).
 
+% By gen server
 init(_DBInfo) ->
   io:format("Init!~n"),
   application:ensure_all_started(mongodb),
@@ -14,4 +16,23 @@ init(_DBInfo) ->
   {ok, Connection} = mc_worker_api:connect([{database, Database}]),
   MyDB = #db_info{connection=Connection},
   {ok, MyDB}.
+
+handle_call(get_emails, _From, MyDB) ->
+  io:format("Retrieving emails...~n"),
+  Connection = MyDB#db_info.connection,
+  Collection = <<"emails">>,
+  {ok, Cursor} = mc_worker_api:find(Connection, Collection, #{}),
+  AllEntries = mc_cursor:rest(Cursor),
+  AllEmails = [maps:get(<<"email">>, Map) || Map <- AllEntries],
+  {reply, AllEmails, MyDB}.
+
+handle_info({ack, _Pid, {error, normal}}, State) -> % This is triggered by m_cursor:rest; it represents no more entries in the db
+  {noreply, State};
+handle_info(Info, State) ->
+  io:format("Received handle request for info ~p state ~p~n", [Info, State]),
+  {noreply, State}.
+
+% By other applications etc.
+get_emails(Pid) ->
+  gen_server:call(Pid, get_emails).
 
