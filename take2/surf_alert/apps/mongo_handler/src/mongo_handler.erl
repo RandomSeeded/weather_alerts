d3268516d4656f037a7c0fb33170c204ab8fe0f6
nate willard <nate.a.willard@gmail.com>
2017-04-06 22:45:21 -0700

    cleanup

--- take2/surf_alert/apps/mongo_handler/src/mongo_handler.erl
+++ take2/surf_alert/apps/mongo_handler/src/mongo_handler.erl
@@ -7,7 +7,7 @@
 % By our supervisor
 start_link() ->
   io:format("Mongo handler start_link!~n"),
-  gen_server:start_link({global, ?MODULE}, ?MODULE, [], []). % I should be able to do gen_server:start_link({local, ?module}), but I can't! Why not?
+  gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 % By gen server
 init(_DBInfo) ->
@@ -45,26 +45,4 @@ get_emails(Pid) ->
 
 add_email(Email, Region) ->
   io:format("worker add_email ~n"),
-  %gen_server:call(?MODULE, {add_email, {Email, Region}}).
-  gen_server:call({global, ?MODULE}, add_email).
-
-% How do we create an externally facing API method?
-% In ppool_server, we were passed a Name, and we invoked gen_server with that name. Named process.
-% Taking in a process seems kinda janky. Why should the caller need to do a start_link? That seems wrong.
-% Possible alternate answer: you might not need to do anything differently. You might not need to talk to the supervisor to do a start_link() and api methods on the worker. Is that really reasonable? Not sure.
-% OK SO: the _app.erl file now is our API. How does that call out to the workers though?
-% OK SO HERE'S HOW IT WORKS
-% You give your process a name. Common convention is {local, ?MODULE}.
-% We then address all future calls to that same name. Wham bam we're done.
-% The API can call this function, which does NOT need to take in the name as a parameter, because it knows the name, because the name is ?MODULE.
-%
-% The way Fred H does it is slightly different; the name for the pool is provided by the user. He then has to address all future calls to the API for the specific sup to reach out to. This is OK, but not my favorite.
-% STILL AN OPEN QUESTION: what happens if we have multiple of these? Does shit break? It seems like it would; we would have two named the same thing, and after we called out to gen_server, it wouldn't know which process to respond to...right?
-% WHAT DOES LOCALLY REGISTERED MEAN??
-% Local vs. global: determines whether the name is accessible to other nodes in the cluster. We still have named this process. This sucks. This sucks this sucks.
-% BUT BUT BUT BUT BUT: just how does a gen_server:call with ?MODULE work anyway? Is that actually a named process?
-% OH SNAP. If ServerName={local,Name}, the gen_server process is registered locally as Name using register/2.
-% The gen_server process...NOT the individual worker process. o.O
-% SO. That means invocations to that same gen_server process WITH THAT NAME may not necessarily go to a specific worker.
-% That may or may not be true. I don't know. But we can test this.
-
+  gen_server:call(?MODULE, add_email).
