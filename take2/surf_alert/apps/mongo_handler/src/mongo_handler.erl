22d59e4c54d293d9680c3d79006d8b45fc0ce9ee
RandomSeeded <nate@blendlabs.com>
2017-04-06 18:05:22 -0700

    working include of dependencies :)

--- take2/surf_alert/apps/mongo_handler/src/mongo_handler.erl
+++ take2/surf_alert/apps/mongo_handler/src/mongo_handler.erl
@@ -11,8 +11,7 @@ init(_DBInfo) ->
   io:format("Init!~n"),
   application:ensure_all_started(mongodb),
   Database = <<"surf_alert">>,
-  % {ok, Connection} = mc_worker_api:connect([{database, Database}]),
-  % MyDB = #db_info{connection=Connection},
-  % {ok, MyDB}.
-  {ok, []}.
+  {ok, Connection} = mc_worker_api:connect([{database, Database}]),
+  MyDB = #db_info{connection=Connection},
+  {ok, MyDB}.
 
