a9c34c17cf24be66f32199a7cbc864bdb923211c
nate willard <nate.a.willard@gmail.com>
2017-04-16 16:35:53 -0700

    start to implement internal run

--- take2/surf_alert/apps/pings/src/pings.erl
+++ take2/surf_alert/apps/pings/src/pings.erl
@@ -2,11 +2,33 @@
 -compile(export_all).
 -behavior(gen_server).
 
+-include("include/surfline_definitions.hrl").
+
 start_link() ->
   gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 init([]) ->
-  Period = {daily, {every, {10, sec}, {between, {1, am}, {11, pm}}}},
+  % Period = {daily, {every, {10, sec}, {between, {1, am}, {11, pm}}}},
+  Period = {daily, {10, am}},
   Job = {io, format, ["Hi ~n"]},
   erlcron:cron({Period, Job}),
   {ok, []}.
+
+internal_run() ->
+  io:format("Checking Forecast~n"),
+  lists:foreach(fun(S) ->
+                    SpotId = S#spot.surfline_spotId,
+                    io:format("SpotId ~p~n", [SpotId])
+                    % Surfline API should totally be its own thing
+                    % {ok, SurflinePid} = surfline_api:start_link(),
+                    % Forecast = surfline_api:check_forecast_good(SurflinePid, SpotId),
+                    % send_emails(Forecast),
+                    % exit(SurflinePid, normal)
+                end, ?Surfline_definitions).
+
+handle_cast(run, State) ->
+  internal_run(),
+  {noreply, State}.
+
+run() ->
+  gen_server:cast(?MODULE, run).
