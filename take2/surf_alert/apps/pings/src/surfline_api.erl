68b7b3fff41a627927c0b2be384288414406f2d0
RandomSeeded <nate@blendlabs.com>
2017-04-18 18:57:08 -0700

    get_forecast start (needs mochijson)

--- take2/surf_alert/apps/pings/src/surfline_api.erl
+++ take2/surf_alert/apps/pings/src/surfline_api.erl
@@ -5,4 +5,24 @@
 start_link(Args) ->
   gen_server:start_link(?MODULE, Args, []).
 
+init({check_forecast, SpotId}) ->
+  ThreeDayForecast = get_forecast(SpotId),
+  {stop, normal, ThreeDayForecast, []}.
 
+get_forecast(SpotId) ->
+  inets:start(),
+  SpotId_Str = integer_to_list(SpotId),
+  Url = "http://api.surfline.com/v1/forecasts/" ++ SpotId_Str ++ "?resources=analysis&days=4",
+  {ok, {{_Version, 200, _ReasonPhrase}, _Headers, Body}} = httpc:request(Url),
+  Struct = mochijson:decode(Body),
+  {struct, JSONData} = Struct,
+  % This needs to actually look it up (lists:keyfind)
+  {"Analysis", {struct, Analysis}} = lists:nth(5, JSONData),
+  {"generalCondition", {array, RegionalForecasts}} = lists:nth(9, Analysis),
+  ThreeDayForecast = lists:nth(4, RegionalForecasts), % Get the forecast for three days out
+  io:format("ThreeDayForecast ~p ~p~n", [ThreeDayForecast, SpotId]),
+  ThreeDayForecast.
+
+check_forecast(SpotId) ->
+  io:format("check forecast spotId ~p~n", [SpotId]),
+  supervisor:start_child(surfline_api_sup, [{check_forecast, SpotId}]).
