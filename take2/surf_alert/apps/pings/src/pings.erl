a11deb8bf5f5e919974d8f6c105e49e243868adc
nate willard <nate.a.willard@gmail.com>
2017-04-19 19:59:17 -0700

    working cron checking all the spots

--- take2/surf_alert/apps/pings/src/pings.erl
+++ take2/surf_alert/apps/pings/src/pings.erl
@@ -8,29 +8,22 @@ start_link(Name) ->
   gen_server:start_link({local, Name}, ?MODULE, Name, []).
 
 init(Name) ->
-  io:format("init name ~p~n", [Name]),
-  Period = {daily, {10, am}},
-  Job = {io, format, ["Hi ~n"]},
+  % Period = {daily, {10, am}},
+  Period = {once, 1},
+  Job = {?MODULE, run, [Name]},
   erlcron:cron({Period, Job}),
   {ok, Name}.
 
-internal_run() ->
-  io:format("Checking Forecast~n"),
-  lists:foreach(fun(S) ->
-                    SpotId = S#spot.surfline_spotId,
-                    io:format("SpotId ~p~n", [SpotId])
-                    % {ok, SurflinePid} = surfline_api:start_link(),
-                    % Forecast = surfline_api:check_forecast_good(SurflinePid, SpotId),
-                    % send_emails(Forecast),
-                    % exit(SurflinePid, normal)
-                end, ?Surfline_definitions).
+internal_run(Name) ->
+  #spot{surfline_spotId = SpotId} = lists:keyfind(Name, #spot.internal_id, ?Surfline_definitions),
+  surfline_api:get_forecast(SpotId).
 
-handle_cast(run, State) ->
-  internal_run(),
-  {noreply, State}.
+handle_cast(run, Name) ->
+  internal_run(Name),
+  {noreply, Name}.
 
-run() ->
-  gen_server:cast(?MODULE, run).
+run(Name) ->
+  gen_server:cast(Name, run).
 
 % OK SO: whats the plan?
 % ISSUES: 
