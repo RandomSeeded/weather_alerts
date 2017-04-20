9001ffc9b0dd620d18a489a39c277f2133b02c77
nate willard <nate.a.willard@gmail.com>
2017-04-19 20:04:16 -0700

    todos

--- take2/surf_alert/apps/pings/src/pings.erl
+++ take2/surf_alert/apps/pings/src/pings.erl
@@ -16,6 +16,10 @@ init(Name) ->
 
 internal_run(Name) ->
   #spot{surfline_spotId = SpotId} = lists:keyfind(Name, #spot.internal_id, ?Surfline_definitions),
+  % TODO (nw): continuation of logic (emails) goes in the runner here
+  % You'll call get_forecast with rpc:async_call (still synchronous internally inside surfline_api)
+  % You'll call get_emails with rpc:async_call (same)
+  % You'll then yield on both. Frickin kickass
   surfline_api:get_forecast(SpotId).
 
 handle_cast(run, Name) ->
