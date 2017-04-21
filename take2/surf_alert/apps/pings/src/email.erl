1fbd9e8f4573b2a01cc16a4d089d8a644ec1071a
nate willard <nate.a.willard@gmail.com>
2017-04-20 20:09:37 -0700

    working email sending

--- take2/surf_alert/apps/pings/src/email.erl
+++ take2/surf_alert/apps/pings/src/email.erl
@@ -9,7 +9,18 @@ start_link(Args) ->
 
 init({send, {EmailAddress, InternalRegionId}}) ->
   io:format("Sending Email to ~p ~p~n", [EmailAddress, InternalRegionId]),
-  {ok, []}.
+  application:start(crypto),
+  application:start(asn1),
+  application:start(public_key),
+  application:start(ssl),
+  {ok, Password} = file:read_file("/home/nate/Projects/surf_alert/take2/surf_alert/apps/pings/priv/.passwords"),
+  gen_smtp_client:send({"surfalertmailer@gmail.com",
+      [EmailAddress], io_lib:format("subject: surf alert\r\nfrom: surf alert daemon\r\nto: ~p\r\n\r\nsurf incoming! http://www.surfline.com/surf-forecasts/northern-california/sf-san-mateo-county_2957", [EmailAddress])},
+    [{relay, "smtp.gmail.com"},
+      {ssl, true},
+      {username, "surfalertmailer@gmail.com"},
+      {password, erlang:binary_to_list(Password)}]),
+  {stop, normal}.
 
 send(EmailAddress, InternalRegionId) ->
   supervisor:start_child(email_sup, [{send, {EmailAddress, InternalRegionId}}]),
