9476bf207bfb18b5d2b995549c42294dba879528
nate willard <nate.a.willard@gmail.com>
2017-04-30 09:26:14 -0700

    change default canonical host url

--- take2/surf_alert/include/config.hrl
+++ take2/surf_alert/include/config.hrl
@@ -1,10 +1,11 @@
 -define(Config, case os:getenv("ENVIRONMENT") of
                   false ->
+                    #{canonical_host_url => "http://surfpings.com",
+                      cron_period => {daily, {10, am}}};
+                  "NATEWILLARD" ->
                     #{canonical_host_url => "http://natewillard.com/projects/surf_alert",
-                      forecast_threshold => 5,
                       cron_period => {daily, {10, am}}};
                   "DEV" ->
                     #{canonical_host_url => "http://localhost:1080",
-                      forecast_threshold => 0,
                       cron_period => {once, 1}}
                 end).
