f0da361cccf30fe091d033907160176baa8ec8c3
nate willard <nate.a.willard@gmail.com>
2017-04-30 19:13:06 -0700

    send emails later in day

--- take2/surf_alert/include/config.hrl
+++ take2/surf_alert/include/config.hrl
@@ -1,10 +1,10 @@
 -define(Config, case os:getenv("ENVIRONMENT") of
                   false ->
                     #{canonical_host_url => "http://surfpings.com",
-                      cron_period => {daily, {10, am}}};
+                      cron_period => {daily, {1, pm}}};
                   "NATEWILLARD" ->
                     #{canonical_host_url => "http://natewillard.com/projects/surf_alert",
-                      cron_period => {daily, {10, am}}};
+                      cron_period => {daily, {1, pm}}};
                   "DEV" ->
                     #{canonical_host_url => "http://localhost:1080",
                       cron_period => {once, 1}}
